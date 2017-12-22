2f3038327879280ad5b39e7228ed4ce484222160
winter mutt <sejongsan@hotmail.com>
2017-12-22 18:37:29 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,9 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element){
-  array = [element, ...array];
-  return array;
+  return [element, ...array];
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-  array.unshift(element);
-  return array;
+  return array.unshift(element);
 }
